63606714bb113b61fa78423d5f1cdc52529d0132
Takuro Niitsuma <takuro.niitsuma@livesense.co.jp>
2018-06-15 14:34:55 +0900

    create helthy stand

---
+++ helthy_stand_bot.py
@@ -0,0 +1,43 @@
+import requests_html
+import requests
+import json
+import os
+import feedparser
+from IPython import embed
+
+SLACK_URL = os.environ['SLACK_URL']
+
+def post_bento_list_to_slack(bento_list):
+    MESSAGE = '今日のヘルシースタンドをお知らせします！ :tada: https://www.healthy-stand-japan.com/blog'
+    
+    attachments = [{
+        "color": "#e4574e",
+        "title": bento["name"],
+        "image_url": bento["img"]
+        } for bento in bento_list]
+    
+    payload = {
+    	"text": MESSAGE,
+    	"attachments": attachments}
+    
+    requests.post(SLACK_URL, json.dumps(payload))
+
+
+def get_bento_list():
+    rss = feedparser.parse('https://www.healthy-stand-japan.com/blog/feed.rss')
+    url = rss['items'][0]['link']
+    session = requests_html.HTMLSession()
+    resp = session.get(url)
+    brands = resp.html.find('h2')[0:2]
+    names = resp.html.find('h3')[0:2]
+    imgs = resp.html.find('p img')[0:2]
+    
+    bentos = [
+    	{"img": img.attrs['src'], "name": name.text + ' (' + brand.text + ')'}
+    	for brand, name, img in zip(brands, names, imgs)]
+    return  bentos
+
+if __name__=='__main__':
+    get_bento_list()
+    bento_list = get_bento_list()
+    post_bento_list_to_slack(bento_list)
