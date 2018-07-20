3d9939f4c7bde5526934dd898af3e847458de3c1
iwtn <yasuaki.iwata@livesense.co.jp>
2018-07-20 21:46:24 +0900

    align spaces and add a test url

--- helthy_stand_bot.py
+++ helthy_stand_bot.py
@@ -8,32 +8,33 @@ SLACK_URL = os.environ['SLACK_URL']
 
 def post_bento_list_to_slack(bento_list):
     MESSAGE = '今日のヘルシースタンドをお知らせします！ :tada: https://www.healthy-stand-japan.com/blog'
-    
+
     attachments = [{
         "color": "#e4574e",
         "title": bento["name"],
         "image_url": bento["img"]
         } for bento in bento_list]
-    
+
     payload = {
-    	"text": MESSAGE,
-    	"attachments": attachments}
-    
+        "text": MESSAGE,
+        "attachments": attachments}
+
     requests.post(SLACK_URL, json.dumps(payload))
 
 
 def get_bento_list():
     rss = feedparser.parse('https://www.healthy-stand-japan.com/blog/feed.rss')
     url = rss['items'][0]['link']
+    url = 'https://www.healthy-stand-japan.com/blog/2018/7/17'
     session = requests_html.HTMLSession()
     resp = session.get(url)
     brands = resp.html.find('h2')[0:2]
     names = resp.html.find('h3')[0:2]
     imgs = resp.html.find('p img')[0:2]
-    
+
     bentos = [
-    	{"img": img.attrs['src'], "name": name.text + ' (' + brand.text + ')'}
-    	for brand, name, img in zip(brands, names, imgs)]
+        {"img": img.attrs['src'], "name": name.text + ' (' + brand.text + ')'}
+        for brand, name, img in zip(brands, names, imgs)]
     return  bentos
 
 if __name__=='__main__':
